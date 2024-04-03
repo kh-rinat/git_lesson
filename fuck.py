684d27078bf17c01856e2d4fc63d0b83bfa2f1a4
Khabipov_Rinat <Rin0-akb@yandex.ru>
2024-04-03 13:19:56 +0300

    one more

--- fuck.py
+++ fuck.py
@@ -1,4 +1,11 @@
-print("fuck")
+import requests
 
 
-print("fickkkkkk")
+api_url = 'http://numbersapi.com/43'
+
+response = requests.get(api_url)
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(response.status_code)
