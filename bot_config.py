917fbe12717e8530ca9986826b7f6d069266826b
Khabipov_Rinat <Rin0-akb@yandex.ru>
2024-04-05 11:34:01 +0300

    one more

--- bot_config.py
+++ bot_config.py
@@ -1,1 +1,6 @@
 git_lesson_bot_API = '7155902927:AAEarb7aKLowABW8yiEsAxg3mAa6mgC7d7w'
+
+url1 = f"https://api.telegram.org/bot{git_lesson_bot_API}/getMe"
+url2 = f"https://api.telegram.org/bot{git_lesson_bot_API}/getUpdates"
+url3 = f"https://api.telegram.org/bot{git_lesson_bot_API}/sendMessage?chat_id=680554251&text=idi_nahyi"
+url4 = 'https://api.telegram.org/bot<token>/sendMessage?chat_id=<chat_id>&text=AMAZING!'
--- fuck.py
+++ fuck.py
@@ -1,11 +1,1 @@
-import time
-
-import requests
-from bot_config import git_lesson_bot_API
-
-url1 = f"https://api.telegram.org/bot{git_lesson_bot_API}/getMe"
-url2 = f"https://api.telegram.org/bot{git_lesson_bot_API}/getUpdates"
-url3 = f"https://api.telegram.org/bot{git_lesson_bot_API}/sendMessage?chat_id=680554251&text=idi_nahyi"
-url4 = 'https://api.telegram.org/bot<token>/sendMessage?chat_id=<chat_id>&text=AMAZING!'
-
-print(url3)
+from aiogram
